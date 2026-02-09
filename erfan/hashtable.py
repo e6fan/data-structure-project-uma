8cd78c03ede99c18dc3c619bb9e3a74e26724f28
Erfan <cpp403@gmail.com>
2026-02-09 12:11:43 +0330

    Add files via upload

---
+++ erfan/hashtable.py
@@ -0,0 +1,36 @@
+class HashTable:
+    def __init__(self, size=10):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def Insert(self, key, value):
+        index = hash(key) % self.size
+        bucket = self.table[index]
+
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket[i] = (key, value)
+                return
+
+        bucket.append((key, value))
+
+    def Search(self, key):
+        index = hash(key) % self.size
+        bucket = self.table[index]
+
+        for k, v in bucket:
+            if k == key:
+                return v
+
+        return None
+
+    def Remove(self, key):
+        index = hash(key) % self.size
+        bucket = self.table[index]
+
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                del bucket[i]
+                return v
+
+        return None
